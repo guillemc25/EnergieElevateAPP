1ebee7af90e42b73f52175b68aaae81b15c18aa8
guillemc25 <guillemmanzanero@gmail.com>
2023-05-10 14:55:28 +0200

    avanzamo

--- app/src/main/java/com/example/energieelevate/activity_registro.kt
+++ app/src/main/java/com/example/energieelevate/activity_registro.kt
@@ -18,41 +18,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 
     }
-    fun RegistrarUsuario() {
-        // inicializamos los views al  código
-        val editTextNombre = findViewById<EditText>(R.id.edtNombre);
-        val editTextApellido= findViewById<EditText>(R.id.edtApellido);
-        val editTextUsuario= findViewById<EditText>(R.id.edtUsuarioReg);
-        val EditTextCorreo= findViewById<EditText>(R.id.edtCorreoReg);
-        val EditTextContraseña= findViewById<EditText>(R.id.edtContra);
 
-        // Obtiene los valores ingresados por el usuario
-        val nombre: String = editTextNombre.text.toString();
-        val apellido: String = editTextApellido.text.toString();
-        val usuario: String = editTextUsuario.text.toString();
-        val correo: String = EditTextCorreo.text.toString()
-        val contraseña: String = EditTextContraseña.text.toString()
-
-        // Crea un objeto ContentValues con los valores a insertar
-        val valores = ContentValues()
-        valores.put("Nombre", nombre)
-        valores.put("Apellido", apellido)
-        valores.put("Usuario", usuario)
-        valores.put("Correo", correo)
-        valores.put("Contraseña", contraseña)
-
-        // Inserta los valores en la tabla Usuarios
-        val miBaseDeDatos =  BaseDeDatos(this)
-        val db: SQLiteDatabase = miBaseDeDatos.getWritableDatabase()
-        val newRowId = db.insert("Usuarios", null, valores)
-
-        // Verifica si la inserción fue exitosa
-        if (newRowId == -1L) {
-            Toast.makeText(this, "Error al registrar usuario", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show()
-            // Aquí puedes agregar código para redirigir al usuario a otra actividad o hacer algo más
-        }
-    }
 
 }
